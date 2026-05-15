af456622e29c5e3d0d603177c06e0f7849764dc7
skbashaop <hussainfractions@gmail.com>
2026-05-15 15:30:44 +0530

    fixed axios

--- src/utils/axiosInstance.js
+++ src/utils/axiosInstance.js
@@ -1,7 +1,7 @@
 import axios from "axios";
 
 const axiosInstance = axios.create({
-  baseURL: "/api" || import.meta.env.VITE_BACKEND_URL || "http://localhost:3000",
+  baseURL: import.meta.env.VITE_BACKEND_URL || "/api" || "http://localhost:3000",
   withCredentials: true,
   headers: {
     "Content-Type": "application/json"
